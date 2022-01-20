a652caf7808475cd5598f4daea192bfe0b91cc4c
jenisakthi <44342960+jenisakthi@users.noreply.github.com>
2022-01-20 10:45:57 +0530

    Update fix_privileges.sh

--- scripts/fix_privileges.sh
+++ scripts/fix_privileges.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-chmod +x /home/ec2-user/server/* jar
-chmod +x /home/ec2-user/server/server_start.sh
-chmod +x /home/ec2-user/server/server_stop.sh
+chmod +x /home/ec2-user/spring_war1/* jar
+chmod +x /home/ec2-user/spring_war1/server_start.sh
+chmod +x /home/ec2-user/spring_war1/server_stop.sh
